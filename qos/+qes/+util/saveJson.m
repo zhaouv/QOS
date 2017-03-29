6b2048fdf8643fe7b25392892a4f916cf112c297
YouWei Zhao <zhao.uv@gmail.com>
2017-03-29 15:51:35 +0800

    saveJson
    version 1 only for one layer
    using python3.4
    >>pyversion

--- qos/+qes/+util/saveJson.m
+++ qos/+qes/+util/saveJson.m
@@ -1,5 +1,5 @@
 function saveJson(fullfilename,fields,value)
-fields = {'ad_boards{1}','numChnls'};
+% fields = {'ad_boards{1}','numChnls'};
 % Copyright 2016 Yulin Wu, USTC
 % mail4ywu@gmail.com/mail4ywu@icloud.com
 
@@ -12,11 +12,30 @@ fields = {'ad_boards{1}','numChnls'};
 %                 29,30,31,32,33,34,35,36,	// rack 5, qubit mw iq
 % 				37,38,39,40 			]  	// rack 6, qubit zpa, right board: power not connected
 % }
+%     error('save fields in json files is not implemented yet.');   
+%     error('save history is not implemented yet.');
+if ischar(value)
+    value=['s"' value '"'];
+elseif isnumeric(value)
+	if numel(value)==1
+        value=['n' num2str(value)];
+    else
+        str='a[';
+        for i=1:numel(value)
+            str=[str num2str(value(i)) ','];
+        end
+        value=[str(1:end-1) ']']; 
+    end
+end
 
+mod = py.importlib.import_module('+qes.+util.saveJson'); 
+py.importlib.reload(mod);    
+result=cell(mod.func1(fullfilename,fields,value));
 
-    error('save fields in json files is not implemented yet.');
-    
-    error('save history is not implemented yet.');
-
-    
+if result{1}== 1
+    error('type error');
+end
+if result{1}== 2
+    error('not a last layer');
+end
 end
