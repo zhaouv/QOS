38f1ec7be8ded595ff5b482d82cbeab32572573b
YulinWu <mail4ywu@gmail.com>
2017-04-18 16:39:00 +0800

    170418-1

--- qos/+qes/+waveform/+fcns/IFFT.m
+++ qos/+qes/+waveform/+fcns/IFFT.m
@@ -1,17 +1,19 @@
-function v = IFFT(obj,f)
-
-
+function fout = IFFT(obj,timeSpan)
 % Copyright 2016 Yulin Wu, University of Science and Technology of China
 % mail4ywu@gmail.com/mail4ywu@icloud.com
 
-%             padln = 2*obj.length;
+    fs = 5+pi;
+%     N = 200+pi;
+%     timeSpan=(1+N)*obj.length;
+%     center=0;
+    center=obj.t0+obj.length/2;
+    L=timeSpan*fs;
+    NFFT = L;
+    t=linspace(center-timeSpan/2,center+timeSpan/2,NFFT);
+    freqs=linspace(-fs/2,fs/2,NFFT);
+    gf=obj(freqs,true);
 
-    error('not implemented');
-
-    padln = 0;
-    t = obj.t0-padln:obj.t0+obj.length - 1+padln;
-    tv = obj.TimeFcn(t);
-    NFFT = 2^nextpow2(numel(t));
-    vi = fft(tv,NFFT);
-    v = exp(-1j*2*pi*f*obj.t0).*interp1(linspace(0,1,NFFT),vi,f);
+    z=ifft(ifftshift(gf));
+%     fout=@(T)fs*spline(t,fftshift(z),T+obj.length/2);%[T0-t0/2 T0+t0/2]
+    fout=@(T)fs*spline(t,fftshift(z),T+obj.length/2);%[T0-t0/2 T0+t0/2]
 end
